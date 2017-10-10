ce016824ada7230e60df2e8c0863ee39d1d76238
Juyeong Lee <Juyeong Lee>
2017-10-10 01:38:48 -0700

    flower image has been created on project 3

--- cpe123/Lab3/lab3.js
+++ cpe123/Lab3/lab3.js
@@ -4,70 +4,115 @@ var isClick;
 function setup() 
 {
    createCanvas(600, 400);
-   background(255);
+
+   //drawing background
+   background(0,143,158,95);
+
+   noStroke();
+   fill(0,33,12);
+   ellipse(-100,450,1400,400);
+   fill(0,69,26);
+   ellipse(-100,450,1300,350);
+   fill(0,107,40);
+   ellipse(-100,450,1200,300);
+
    isClick = false;
 }
 
 function drawFlower()
 {
+   //flower stem
    push();
-      translate(300,150)
+      translate(300,150);
 
-      //stem of the flower - bezierVertex
-      /*
+      fill(4,135,0);
+      noStroke();
+      rect(-10,0,20,300);
+   pop();
+
+   //pallet of the flower
+   fill(255,187,0);
+   push();
+      translate(300,150);
+      ellipse(0,0,40,40);
+
+      for(var i = 0; i < 9*PI/4; i++)
+      {
+         rotate(i*PI/4);
          beginShape();
-            bezierVertex();
+            curveVertex(40,0);
+            curveVertex(40,0);
+            curveVertex(120,-20);
+            curveVertex(160,0);
+            curveVertex(120,20)
+            curveVertex(40,0);
+            curveVertex(40,0);
          endShape();
-      */
-
-      //pallet of the flower 1 - bezierVertex
-      /*
-         for(i=0;i<2*PI;i=+PI/8){
-            push();
-               bezierVertex();
-               rotate();
-            pop();
-         }
-      */
-
-      //pallet of the flower 2 - bezierVertex
-
-
-      //the middle part of the flower
-      fill(125,50,0);
-      ellipse(0,0,100,100);
-
-      //texture of the sunflower
-      /*
-      translate to one edge of the flower
-      bezier starts with the strong curve to the left
-      for(i=0;i<10;i++){
-         as the line move left to right, the curve curves to the right
       }
-      */
 
+      rotate(PI/8);
+
+      for(var i = 0; i < 9*PI/4; i++)
+      {
+         rotate(i*PI/4);
+         beginShape();
+            curveVertex(40,0);
+            curveVertex(40,0);
+            curveVertex(120,-20);
+            curveVertex(160,0);
+            curveVertex(120,20)
+            curveVertex(40,0);
+            curveVertex(40,0);
+         endShape();
+      }
+   pop();
+
+   //the head of the flower
+   push();
+      noStroke();
+      translate(300,150);   
+      fill(92,51,0);
+      ellipse(0,0,150);
+   pop();
+
+   //leaves of the flower
+   push();   
+      translate(300,350);   
+      fill(4,135,0);
+
+      beginShape();
+         vertex(0,0);
+         bezierVertex(40,0,40,-75,160,0);
+         bezierVertex(50,80,60,25,0,0);
+      endShape();
+
+      scale(-1,1);
+      beginShape();
+         vertex(0,0);
+         bezierVertex(40,0,40,-75,160,0);
+         bezierVertex(50,80,60,25,0,0);
+      endShape();
    pop();
 }
 
 function drawEye()
 {
    //the pupil of the eye - bezierVertex
-   /*while(i<10 && isClick){
-      creating effect to make eye open up slowly
+   while(i<10 && isClick){
+      
    }
    while(i>10 && !isClick){
       eye is closed slowly
    }
-   */
-}
-
-function whileClicked()
-{
-   isClick = !isClick;
 }
 
 function draw() 
 {
    drawFlower();
+}
+
+function whileClicked()
+{
+   isClick = !isClick;
    drawEye();
 }
